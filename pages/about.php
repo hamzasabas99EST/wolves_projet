a11c59fa62590c856709e3c230e6b6dc1808cf8a
unknown <hamzasabas99@gmail.com>
2020-10-27 14:47:33 +0100

    premier jour

---
+++ pages/about.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The wolves</title>
+    <link rel="stylesheet" href="../CSS/style.css">
+
+</head>
+<body>
+    <div class="grid-container">
+        <div class="grid-item">
+            <img src="../images/centre.jpg" alt="" width="400px" height="300px">
+        </div>
+        <div class="grid-item">
+           <img src="../images/terrains.jpg" alt=""  width="400px" height="300px">
+        </div> 
+        <div class="grid-item">
+            <img src="../images/employee.jpg" alt=".." width="400px" height="300px">
+        </div>
+    </div>
+</body>
+</html>
---
+++ pages/services.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The wolves</title>
+    <link rel="stylesheet" href="../CSS/style.css">
+</head>
+<body>
+    <div class="container0">
+        <div class="services">
+ 
+            <div class="services_boxs">
+                <div class="ser">
+                    <img alt="medical" src="../images/medical.png" class="imag"/>
+                    <h1 class="titleh1">Service Médical</h1>
+                    <p class="paragraph">Nos centres disposent d'infermiers pour les soins medicaux.</p>
+                </div>
+                <div class="ser">
+                    <img alt="vestaire" src="../images/vestaire.png" class="imag"/>
+                    <h1 class="titleh1">Vestaires</h1>
+                    <p class="paragraph">Chaque terrain dispose deux vestaire pour se changer .</p>
+                </div>
+                <div class="ser">
+                    <img alt="douche" src="../images/douche.png" class="imag"/>
+                        <h1 class="titleh1">Douches</h1>
+                    <p class="paragraph">Des douches sont disponible dans chaque centre. </p>
+                </div>
+                <div class="ser">
+                    <img alt="nettoyage" src="../images/nettoyage.jpg" class="imag"/>
+                        <h1 class="titleh1">service nettoyage</h1>
+                    <p class="paragraph">Chaque centre dispose d'un personnel d'hygiène pour le confort des visiteurs. </p>
+                </div>
+                <div class="ser">
+                    <img alt="arrousage" src="../images/arrousage.jpg" class="imag"/>
+                        <h1 class="titleh1">Arrousage</h1>
+                    <p class="paragraph">
+                        Le centre assure un entrertien régulier
+                        de la pelouse pour combiner esthétique et confort.
+                    </p>
+                </div>
+                <div class="ser">
+                    <img alt="soccer" src="../images/soccer.png" class="imag"/>
+                        <h1 class="titleh1">Les equipements sportif(s)</h1>
+                    <p class="paragraph">Des équipements sont mis disposition:ballons-tenus-dossard...  . </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
