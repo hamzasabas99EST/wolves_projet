825951756afce48430f4c7274aedbffec31345fe
unknown <hamzasabas99@gmail.com>
2020-10-29 00:58:49 +0100

    modification page about

--- pages/about.php
+++ pages/about.php
@@ -3,21 +3,72 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+
     <title>The wolves</title>
     <link rel="stylesheet" href="../CSS/style.css">
+    
 
 </head>
-<body>
-    <div class="grid-container">
-        <div class="grid-item">
-            <img src="../images/centre.jpg" alt="" width="400px" height="300px">
+<body class="about_page">
+        <div class="abt-container">
+            <img src="../images/centre.jpg" alt=""  class="avatar">
+            <h1 class="title_about">Hello</h1>
+            <p class="paragraph_abt">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis beatae quas dicta et! Aperiam harum dolorum culpa
+                possimus tempora aspernatur soluta error odio nulla facilis, asperiores eos delectus ipsam vel! Lorem ipsum dolor sit 
+                amet consectetur adipisicing elit.Quo perferendis, hic aspernatur dignissimos dolorem consequuntur? Rerum, temporibus
+                nam! Voluptate rerum,perferendis qui quia vero voluptates eaque eum similique ullam fugiat!
+            </p>
         </div>
-        <div class="grid-item">
-           <img src="../images/terrains.jpg" alt=""  width="400px" height="300px">
-        </div> 
-        <div class="grid-item">
-            <img src="../images/employee.jpg" alt=".." width="400px" height="300px">
+        <div class="abt-container">
+            <img src="../images/centre.jpg" alt=""  class="avatar" style="float:left;">
+            <h1 class="title_about">Hello</h1>
+            <p class="paragraph_abt">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis beatae quas dicta et! Aperiam harum dolorum culpa
+                possimus tempora aspernatur soluta error odio nulla facilis, asperiores eos delectus ipsam vel! Lorem ipsum dolor sit 
+                amet consectetur adipisicing elit.Quo perferendis, hic aspernatur dignissimos dolorem consequuntur? Rerum, temporibus
+                nam! Voluptate rerum,perferendis qui quia vero voluptates eaque eum similique ullam fugiat!
+            </p>
         </div>
-    </div>
+        <div class="abt-container">
+            <img src="../images/centre.jpg" alt=""  class="avatar">
+            <h1 class="title_about">Hello</h1>
+            <p class="paragraph_abt">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis beatae quas dicta et! Aperiam harum dolorum culpa
+                possimus tempora aspernatur soluta error odio nulla facilis, asperiores eos delectus ipsam vel! Lorem ipsum dolor sit 
+                amet consectetur adipisicing elit.Quo perferendis, hic aspernatur dignissimos dolorem consequuntur? Rerum, temporibus
+                nam! Voluptate rerum,perferendis qui quia vero voluptates eaque eum similique ullam fugiat!
+            </p>
+        </div>
+
 </body>
 </html>
+
+
+    <!--div class="grid-container">
+        <div class="grid-item">
+            <img class="avatar" src="../images/centre.jpg" alt="" width="400px" height="300px">
+            <p class="paragraph_abt">
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+             laboriosam dignissimos aperiam quod, molestias iste optio amet. Voluptatem, ratione 
+             accusantium? Quidem placeat error eos nemo eux m reiciendis, illo ipsam consequatur.
+            </p>
+        </div>
+
+        <div class="grid-item">
+           <img class="avatar" src="../images/terrains.jpg" alt=""  width="400px" height="300px">
+           <p class="paragraph_abt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+             laboriosam dignissimos aperiam quod, molestias iste optio amet. Voluptatem, ratione 
+             accusantium? Quidem placeat error eos nemo eum reiciendis, illo ipsam consequatur.
+            </p>
+        </div>
+
+        <div class="grid-item">
+            <img class="avatar" src="../images/employee.jpg" alt=".." width="400px" height="300px">
+            <p class="paragraph_abt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+             laboriosam dignissimos aperiam quod, molestias iste optio amet. Voluptatem, ratione 
+             accusantium? Quidem placeat error eos nemo eum reiciendis, illo ipsam consequatur.
+            </p>
+        </div>
+    </div-->
